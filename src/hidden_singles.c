07c7db4901d67d01f5321425cb282f5fb09530b7
KhoiNguyenVGU <10422058@student.vgu.edu.vn>
2023-12-19 16:57:58 +0700

    commit

--- src/hidden_singles.c
+++ src/hidden_singles.c
@@ -1,23 +1,17 @@
 #include "hidden_singles.h"
 #include <stdlib.h>
 
-int find_hidden_single_values(Cell **p_cells, int *hidden_single_values)
+int find_hidden_single_values(Cell **p_cells, int *hidden_single_values) 
 {
     int num_out = 0;
+    int candidate_counter[BOARD_SIZE] = {0};
 
-    int candidate_counter[BOARD_SIZE];
-
-    for (int i = 0; i < BOARD_SIZE; i++)
+    for (int i = 0; i < BOARD_SIZE; i++) 
     {
-        candidate_counter[i] = 0;
-    }
-
-    for (int i = 0; i < BOARD_SIZE; i++)
-    {
-        if (p_cells[i]->num_candidates > 1)
+        if (p_cells[i]->num_candidates > 1) 
         {
             int *candidates = get_candidates(p_cells[i]);
-            for (int j = 0; j < p_cells[i]->num_candidates; j++)
+            for (int j = 0; j < p_cells[i]->num_candidates; j++) 
             {
                 candidate_counter[candidates[j] - 1]++;
             }
@@ -25,21 +19,21 @@ int find_hidden_single_values(Cell **p_cells, int *hidden_single_values)
         }
     }
 
-    for (int i = 0; i < BOARD_SIZE; i++)
+    for (int i = 0; i < BOARD_SIZE; i++) 
     {
-        if (candidate_counter[i] == 1)
-        {
+        if (candidate_counter[i] == 1) {
             hidden_single_values[num_out++] = i + 1;
         }
     }
+
     return num_out;
 }
 
-bool is_in_list_hidden_singles(HiddenSingle *p_array, int size, Cell *p)
+bool is_in_list_hidden_singles(HiddenSingle *p_array, int size, Cell *p) 
 {
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++) 
     {
-        if (p_array[i].p_cell == p)
+        if (p == p_array[i].p_cell) 
         {
             return true;
         }
@@ -47,24 +41,24 @@ bool is_in_list_hidden_singles(HiddenSingle *p_array, int size, Cell *p)
     return false;
 }
 
-void find_hidden_single(Cell **p_cells, HiddenSingle *p_hidden_singles, int *p_counter)
+void find_hidden_single(Cell **p_cells, HiddenSingle *p_hidden_singles, int *p_counter) 
 {
     int hidden_single_values[BOARD_SIZE];
     int num_values = find_hidden_single_values(p_cells, hidden_single_values);
 
-    for (int i = 0; i < num_values; i++)
+    for (int i = 0; i < num_values; i++) 
     {
-        for (int j = 0; j < BOARD_SIZE; j++)
+        for (int j = 0; j < BOARD_SIZE; j++) 
         {
-            if (p_cells[j]->num_candidates > 1)
+            if (p_cells[j]->num_candidates > 1) 
             {
                 int *candidates = get_candidates(p_cells[j]);
-                for (int k = 0; k < p_cells[j]->num_candidates; k++)
+                for (int k = 0; k < p_cells[j]->num_candidates; k++) 
                 {
-                    if ((candidates[k] == hidden_single_values[i]) && (!is_in_list_hidden_singles(p_hidden_singles, *p_counter, p_cells[j])))
+                    if (candidates[k] == hidden_single_values[i] && !is_in_list_hidden_singles(p_hidden_singles, *p_counter, p_cells[j])) 
                     {
-                        p_hidden_singles[(*p_counter)++] =
-                            (HiddenSingle){p_cells[j], hidden_single_values[i]};
+                        p_hidden_singles[*p_counter] = (HiddenSingle){p_cells[j], hidden_single_values[i]};
+                        (*p_counter)++;
                     }
                 }
                 free(candidates);
@@ -73,21 +67,22 @@ void find_hidden_single(Cell **p_cells, HiddenSingle *p_hidden_singles, int *p_c
     }
 }
 
-int hidden_singles(SudokuBoard *p_board)
+int hidden_singles(SudokuBoard *p_board) 
 {
     int hidden_single_cells_counter = 0;
     HiddenSingle hidden_single_cells[BOARD_SIZE * BOARD_SIZE];
 
-    for (int i = 0; i < BOARD_SIZE; i++)
+    for (int i = 0; i < BOARD_SIZE; i++) 
     {
         find_hidden_single(p_board->p_rows[i], hidden_single_cells, &hidden_single_cells_counter);
         find_hidden_single(p_board->p_cols[i], hidden_single_cells, &hidden_single_cells_counter);
         find_hidden_single(p_board->p_boxes[i], hidden_single_cells, &hidden_single_cells_counter);
     }
 
-    for (int i = 0; i < hidden_single_cells_counter; i++)
+    for (int i = 0; i < hidden_single_cells_counter; i++) 
     {
-        set_candidates(hidden_single_cells[i].p_cell, (int *)(int[]){hidden_single_cells[i].value}, 1);
+        int single_value = hidden_single_cells[i].value;
+        set_candidates(hidden_single_cells[i].p_cell, &single_value, 1);
     }
 
     return hidden_single_cells_counter;
--- src/naked_pairs.c
+++ src/naked_pairs.c
@@ -1,6 +1,89 @@
 #include "naked_pairs.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+bool is_in_list_naked_pairs(NakedPairs *p_array, Cell *p)
+{
+    for (int i = 0; i < BOARD_SIZE * BOARD_SIZE; i++)
+    {
+        if ((p_array[i].p_cell_1 == p) || (p_array[i].p_cell_2 == p))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+void find_naked_pairs(Cell **p_cells, int *p_counter, int *p_unset, UnsetData *p_unset_array, NakedPairs *p_naked_pairs)
+{
+    for (int i = 0; i < BOARD_SIZE - 1; i++)
+    {
+        if (p_cells[i]->num_candidates == 2)
+        {
+            for (int j = i + 1; j < BOARD_SIZE; j++)
+            {
+                if (p_cells[j]->num_candidates == 2)
+                {
+                    int *candidates_1 = get_candidates(p_cells[i]);
+                    int *candidates_2 = get_candidates(p_cells[j]);
+                    if ((candidates_1[0] == candidates_2[0]) && (candidates_1[1] == candidates_2[1]))
+                    {
+                        for (int k = 0; k < BOARD_SIZE; k++)
+                        {
+                            if ((k != i) && (k != j) && (p_cells[k]->num_candidates > 1))
+                            {
+                                if (is_candidate(p_cells[k], candidates_1[0]) || is_candidate(p_cells[k], candidates_1[1]))
+                                {
+                                    p_unset_array[*p_unset].p_cell = p_cells[k];
+                                    p_unset_array[*p_unset].candidate_1 = candidates_1[0];
+                                    p_unset_array[*p_unset].candidate_2 = candidates_1[1];
+                                    (*p_unset)++;
+                                }
+                            }
+                            if (!is_in_list_naked_pairs(p_naked_pairs, p_cells[i]))
+                            {
+                                p_naked_pairs[*p_counter].p_cell_1 = p_cells[i];
+                                p_naked_pairs[*p_counter].p_cell_2 = p_cells[j];
+                                (*p_counter)++;
+                            }
+                        }
+                        
+                    }
+                    free(candidates_1);
+                    free(candidates_2);
+                
+                }
+            }
+        }
+    }
+}
 
 int naked_pairs(SudokuBoard *p_board)
 {
-    return 0;
+    NakedPairs naked_pairs[BOARD_SIZE * BOARD_SIZE];
+    int naked_counter = 0;
+    UnsetData unset_array[BOARD_SIZE * BOARD_SIZE];
+    int unset_counter = 0;
+
+    for (int i = 0; i < BOARD_SIZE; i++)
+    {
+        find_naked_pairs(p_board->p_rows[i], &naked_counter, &unset_counter, unset_array, naked_pairs);
+        find_naked_pairs(p_board->p_cols[i], &naked_counter, &unset_counter, unset_array, naked_pairs);
+        find_naked_pairs(p_board->p_boxes[i], &naked_counter, &unset_counter, unset_array, naked_pairs);
+    }
+
+    for (int i = 0; i < unset_counter; i++)
+    {
+        if (is_candidate(unset_array[i].p_cell, unset_array[i].candidate_1))
+        {
+            unset_candidate(unset_array[i].p_cell, unset_array[i].candidate_1);
+        }
+        if (is_candidate(unset_array[i].p_cell, unset_array[i].candidate_2))
+        {
+            unset_candidate(unset_array[i].p_cell, unset_array[i].candidate_2);
+        }
+    }
+    printf("Naked pairs: %d\n", naked_counter);
+    return naked_counter;
 }
